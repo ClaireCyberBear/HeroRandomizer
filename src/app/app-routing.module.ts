556bcd84eab089d50c87b8d1302d005ca8283fd0
ClaireCyberBear <clairecyberbear@gmail.com>
2023-07-24 16:32:31 -0500

    Routed so that /hero-select is always default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HeroSelectComponent } from './hero-select/hero-select.component';
+import { HeroComponent } from './hero/hero.component';
 
 const routes: Routes = [
   { path: 'hero-select', component: HeroSelectComponent },
+  { path: 'hero/:id', component: HeroComponent },
+  { path: '', redirectTo: '/hero-select', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ReactiveFormsModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,6 @@ import { HeroComponent } from './hero/hero.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [
-  { path: 'hero-select', component: HeroSelectComponent },
-  { path: 'hero/:id', component: HeroComponent },
-  { path: '', redirectTo: '/hero-select', pathMatch: 'full' },
-];
-
 @NgModule({
   declarations: [AppComponent, HeroSelectComponent, HeroComponent],
   imports: [
